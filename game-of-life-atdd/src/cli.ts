f904154ab75f570319ab0f84cfa476c1925dbe96
Sergio Álvarez <sergio@codecoolture.com>
2019-11-18 09:18:59 +0100

    Create a new Board entity and move some logic there

--- game-of-life-atdd/src/cli.ts
+++ game-of-life-atdd/src/cli.ts
@@ -1,47 +1,24 @@
+import { writeFileSync } from "fs";
 import { options } from "yargs";
-import { readFileSync, writeFileSync } from "fs";
+import { Board } from "./Board";
 
-const argv = options({
-  input: { type: "string", demand: true },
-  output: { type: "string", demand: true },
-}).argv;
-
-const inputBoard = readFileSync(argv.input, { encoding: "utf-8" });
-
-const matrix = inputBoard
-  .trim()
-  .split("\n")
-  .map((row) => row.split(" ").map((cell) => parseInt(cell, 10)));
-
-const result = matrix.map((row, rowIndx) => {
-  return row.map((cell, cellIndx) => {
-    const neigh = [
-      matrix[rowIndx - 1] && matrix[rowIndx - 1][cellIndx - 1],
-      matrix[rowIndx - 1] && matrix[rowIndx - 1][cellIndx],
-      matrix[rowIndx - 1] && matrix[rowIndx - 1][cellIndx + 1],
-      matrix[rowIndx][cellIndx - 1],
-      matrix[rowIndx][cellIndx + 1],
-      matrix[rowIndx + 1] && matrix[rowIndx + 1][cellIndx - 1],
-      matrix[rowIndx + 1] && matrix[rowIndx + 1][cellIndx],
-      matrix[rowIndx + 1] && matrix[rowIndx + 1][cellIndx + 1],
-    ];
-
-    const numberOfAliveCells = neigh.filter(Boolean).length;
-
-    if (numberOfAliveCells < 2) {
-      return 0;
-    }
-
-    if (numberOfAliveCells > 3) {
-      return 0;
-    }
-
-    if (numberOfAliveCells === 3 && !cell) {
-      return 1;
-    }
-
-    return cell;
+function App() {
+  const { argv } = options({
+    input: { type: "string", demand: true },
+    output: { type: "string", demand: true },
   });
-});
 
-writeFileSync(argv.output, result.map((row) => row.join(" ")).join("\n"), { encoding: "utf-8" });
+  const board = Board.fromFile(argv.input);
+
+  writeFileSync(
+    argv.output,
+    board
+      .evolve()
+      .toArray()
+      .map((row) => row.join(" "))
+      .join("\n"),
+    { encoding: "utf-8" },
+  );
+}
+
+App();
