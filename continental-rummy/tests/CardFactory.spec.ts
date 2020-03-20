55fb4305f7c0a7806d9d7205c6b02560ed049ba0
Sergio Álvarez <sergio@codecoolture.com>
2020-03-20 21:50:20 +0100

    Avoid redundant tests

--- continental-rummy/tests/CardFactory.spec.ts
+++ continental-rummy/tests/CardFactory.spec.ts
@@ -13,134 +13,35 @@ import { Jack } from "../src/cards/Jack";
 import { Queen } from "../src/cards/Queen";
 import { King } from "../src/cards/King";
 import { Joker } from "../src/cards/Joker";
+import { CardOptions } from "../src/Card";
+
+function testCardFactory({ rank, options, type }: { rank: string; options: CardOptions; type: any }) {
+  it(`returns ${type.name} for "${rank}"`, () => {
+    const subject = new CardFactory();
+
+    const result = subject.build(rank, options);
+
+    expect(result).toBeInstanceOf(type);
+    expect(result.suit).toEqual(options.suit);
+  });
+}
 
 describe("CardFactory", () => {
   describe("#build", () => {
-    it("returns the right Card instance for Aces", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("A", { suit: "Hearts" });
-
-      expect(result).toBeInstanceOf(Ace);
-      expect(result.suit).toEqual("Hearts");
-    });
-
-    it("returns the right Card instance for Two", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("2", { suit: "Diamonds" });
-
-      expect(result).toBeInstanceOf(Two);
-      expect(result.suit).toEqual("Diamonds");
-    });
-
-    it("returns the right Card instance for Three", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("3", { suit: "Diamonds" });
-
-      expect(result).toBeInstanceOf(Three);
-      expect(result.suit).toEqual("Diamonds");
-    });
-
-    it("returns the right Card instance for Four", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("4", { suit: "Diamonds" });
-
-      expect(result).toBeInstanceOf(Four);
-      expect(result.suit).toEqual("Diamonds");
-    });
-
-    it("returns the right Card instance for Five", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("5", { suit: "Diamonds" });
-
-      expect(result).toBeInstanceOf(Five);
-      expect(result.suit).toEqual("Diamonds");
-    });
-
-    it("returns the right Card instance for Six", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("6", { suit: "Clubs" });
-
-      expect(result).toBeInstanceOf(Six);
-      expect(result.suit).toEqual("Clubs");
-    });
-
-    it("returns the right Card instance for Seven", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("7", { suit: "Clubs" });
-
-      expect(result).toBeInstanceOf(Seven);
-      expect(result.suit).toEqual("Clubs");
-    });
-
-    it("returns the right Card instance for Eight", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("8", { suit: "Clubs" });
-
-      expect(result).toBeInstanceOf(Eight);
-      expect(result.suit).toEqual("Clubs");
-    });
-
-    it("returns the right Card instance for Nine", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("9", { suit: "Clubs" });
-
-      expect(result).toBeInstanceOf(Nine);
-      expect(result.suit).toEqual("Clubs");
-    });
-
-    it("returns the right Card instance for Ten", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("10", { suit: "Clubs" });
-
-      expect(result).toBeInstanceOf(Ten);
-      expect(result.suit).toEqual("Clubs");
-    });
-
-    it("returns the right Card instance for Jack", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("J", { suit: "Spades" });
-
-      expect(result).toBeInstanceOf(Jack);
-      expect(result.suit).toEqual("Spades");
-    });
-
-    it("returns the right Card instance for Queen", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("Q", { suit: "Diamonds" });
-
-      expect(result).toBeInstanceOf(Queen);
-      expect(result.suit).toEqual("Diamonds");
-    });
-
-    it("returns the right Card instance for King", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("K", { suit: "Clubs" });
-
-      expect(result).toBeInstanceOf(King);
-      expect(result.suit).toEqual("Clubs");
-    });
-
-    it("returns the right Card instance for Joker", () => {
-      const subject = new CardFactory();
-
-      const result = subject.build("∞", { suit: "None" });
-
-      expect(result).toBeInstanceOf(Joker);
-      expect(result.suit).toEqual("None");
-    });
+    testCardFactory({ rank: "A", options: { suit: "Hearts" }, type: Ace });
+    testCardFactory({ rank: "2", options: { suit: "Diamonds" }, type: Two });
+    testCardFactory({ rank: "3", options: { suit: "Diamonds" }, type: Three });
+    testCardFactory({ rank: "4", options: { suit: "Diamonds" }, type: Four });
+    testCardFactory({ rank: "5", options: { suit: "Diamonds" }, type: Five });
+    testCardFactory({ rank: "6", options: { suit: "Diamonds" }, type: Six });
+    testCardFactory({ rank: "7", options: { suit: "Clubs" }, type: Seven });
+    testCardFactory({ rank: "8", options: { suit: "Clubs" }, type: Eight });
+    testCardFactory({ rank: "9", options: { suit: "Clubs" }, type: Nine });
+    testCardFactory({ rank: "10", options: { suit: "Clubs" }, type: Ten });
+    testCardFactory({ rank: "J", options: { suit: "Spades" }, type: Jack });
+    testCardFactory({ rank: "Q", options: { suit: "Diamonds" }, type: Queen });
+    testCardFactory({ rank: "K", options: { suit: "Clubs" }, type: King });
+    testCardFactory({ rank: "∞", options: { suit: "None" }, type: Joker });
 
     it("throws an exception for unknown cards", () => {
       const subject = new CardFactory();
