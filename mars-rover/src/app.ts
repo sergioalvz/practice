d32d67b76f6f346fdc7ea1c10133b491c3be6d25
Sergio Álvarez <sergio@codecoolture.com>
2022-03-14 18:40:37 +0100

    Iterate over commands rather than rovers

--- mars-rover/src/app.ts
+++ mars-rover/src/app.ts
@@ -4,8 +4,8 @@ export class App {
   constructor(private rovers: Rover[]) {}
 
   public run(commands: string[]): string[] {
-    return this.rovers.map((rover, index) => {
-      const command = commands[index];
+    return commands.map((command, index) => {
+      const rover = this.rovers[index];
 
       if (command === "") {
         return rover.toString();
